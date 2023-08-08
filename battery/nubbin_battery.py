f9f7edc273ad7eaf11d46d46ad3f192f662d23fe
sinan <nanossn1008@gmail.com>
2023-08-08 14:30:36 +0800

    completed software architecture and refactoring.

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,11 @@
+from .battery import Battery
+
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.__current_date = current_date
+        self.__last_service_date = last_service_date
+
+    def needs_service(self):
+        service_threshold_date = self.__last_service_date.replace(year=self.__last_service_date.year + 4)
+        return service_threshold_date < self.__current_date
